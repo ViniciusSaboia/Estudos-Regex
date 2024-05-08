e48750bf531fc597a871c840891ce31379cee4e7
Vinicius Saboia <viniciuscsaboia@gmail.com>
2024-05-08 11:19:43 -0300

    fix

--- buscas-csv/busca-email.js
+++ buscas-csv/busca-email.js
@@ -2,6 +2,6 @@ const fs = require('fs')
 const bancoCsv = 'database.csv'
 const banco = fs.readFileSync(bancoCsv, "utf-8")
 
-const patternEmail = /\w+@\w+.\w+.\w+/g
+const patternEmail = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/gm
 const matchEmail = banco.match(patternEmail)
 console.log(matchEmail);
